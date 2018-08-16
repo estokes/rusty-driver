ed0b80da6b51c075f93ba57390e8bf65ae977214
Eric Stokes <letaris@gmail.com>
2018-08-16 14:29:31 -0400

    add wikipedia search example

--- examples/wikipedia.rs
+++ examples/wikipedia.rs
@@ -12,18 +12,22 @@ use rusty_driver::{Locator, Driver, error::*};
 use std::{env::args, process::exit};
 
 #[async]
-fn curl(url: String) -> Result<()> {
+fn wikipedia(article: String) -> Result<()> {
     let driver = await!(Driver::new("http://localhost:4444".into(), None))?;
-    await!(driver.clone().goto(url))?;
-    let body = await!(driver.clone().find(Locator::Css("body".into()), None))?;
-    println!("{}", await!(driver.html(body, true))?);
+    await!(driver.clone().goto("https://www.wikipedia.org".into()))?;
+    let search = await!(
+        driver.clone().find(Locator::Css("form#search-form".into()), None)
+    )?;
+    await!(driver.clone().set_by_name(search.clone(), "search".into(), article))?;
+    await!(driver.clone().submit(search))?;
+    println!("{}", await!(driver.source())?);
     Ok(())
 }
 
 fn main() {
-    if let Some(url) = args().nth(1) {
+    if let Some(article) = args().nth(1) {
         run(async_block! {
-            await!(curl(url)).map_err(|e| eprintln!("error: {}", e))
+            await!(wikipedia(article)).map_err(|e| eprintln!("error: {}", e))
         })
     } else {
         eprintln!("usage curl: <url>");
